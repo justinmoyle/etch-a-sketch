792903a9305abf758d1ac4f16cbe72d36ca81fd6
justinmoyle <195149107+justinmoyle@users.noreply.github.com>
2025-02-17 19:55:53 -0500

    create a 16x16 grid of divs

---
+++ java.js
@@ -0,0 +1,9 @@
+const container = document.getElementById('gridContainer');
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridContainer.appendChild(gridItem);
+    }
+}
